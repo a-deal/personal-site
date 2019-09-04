d2dac0c8c0a83783de13bbe25f604dd1b86afac4
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-04 16:32:57 +0300

    Include Theme component for colors

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react'
 import { Normalize } from 'styled-normalize'
 import styled from 'styled-components'
 
+import { Theme } from '../../styles/Theme'
 import { GlobalStyle } from '../../styles/Global'
 
 const StyledMain = styled.main`
@@ -10,10 +11,12 @@ const StyledMain = styled.main`
 
 export const Container = (props: PropsWithChildren<{}>) => {
   return (
-    <StyledMain>
-      <Normalize />
-      <GlobalStyle />
-      {props.children}
-    </StyledMain>
+    <Theme>
+      <StyledMain>
+        <Normalize />
+        <GlobalStyle />
+        {props.children}
+      </StyledMain>
+    </Theme>
   )
 }
