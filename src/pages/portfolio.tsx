838be912bd279f4d753cc1727d252e1c21a0ba78
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-09 23:31:11 +0300

    Add preliminary portfolio cards

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import TwitchLogo from '../images/twitch_logo.png'
+import OverLogo from '../images/over_logo.jpg'
+import GMULogo from '../images/gmu_logo.png'
+import OxfordLogo from '../images/oxford_logo.jpg'
 import { Shell } from '../components/Layout/Shell'
 import Helmet from 'react-helmet'
 
@@ -17,6 +21,22 @@ const StyledPortfolioItem = styled.div<{ gridArea: string }>`
   justify-content: center;
 `
 
+const StyledCard = styled.div`
+  background-color: white;
+  box-shadow: 0 16px 32px 0 rgba(0, 0, 0, 0.08);
+  border: 1px solid #dedede;
+  border-radius: 8px;
+  height: 320px;
+  padding: 8px;
+  width: 280px;
+`
+
+const StyledCardLogo = styled.img`
+  border-radius: 8px;
+  height: 50px;
+  width: 50px;
+`
+
 const Portfolio = () => {
   return (
     <Shell>
@@ -25,12 +45,32 @@ const Portfolio = () => {
       </Helmet>
 
       <StyledPortfolioContainer>
-        <StyledPortfolioItem gridArea={'1 / 1 / 2 / 2'}>Over</StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'1 / 2 / 2 / 3'}>Twitch</StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'1/ 3 / 2 / 4'}>Your company</StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'2 / 1 / 3 / 2'}>George Mason University</StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'2 / 2 / 3 / 3'}>Oxford University</StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'2 / 3 / 3 / 4'}>Current areas of interest</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'1 / 1 / 2 / 2'}>
+          <StyledCard>
+            <StyledCardLogo src={OverLogo} />
+          </StyledCard>
+        </StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'1 / 2 / 2 / 3'}>
+          <StyledCard>
+            <StyledCardLogo src={TwitchLogo} />
+          </StyledCard>
+        </StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'1/ 3 / 2 / 4'}>
+          <StyledCard>Your Company</StyledCard>
+        </StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 1 / 3 / 2'}>
+          <StyledCard>
+            <StyledCardLogo src={GMULogo} />
+          </StyledCard>
+        </StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 2 / 3 / 3'}>
+          <StyledCard>
+            <StyledCardLogo src={OxfordLogo} />
+          </StyledCard>
+        </StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 3 / 3 / 4'}>
+          <StyledCard>Current Areas of Interest</StyledCard>
+        </StyledPortfolioItem>
       </StyledPortfolioContainer>
     </Shell>
   )
