06437d02a0311437fee83301dcae2062a0945fd7
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-09 22:29:11 +0300

    Add container scaffold for portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Shell } from '../components/Layout/Shell'
 import Helmet from 'react-helmet'
+
+const StyledPortfolioContainer = styled.div`
+  display: grid;
+  grid-template: 2 / 3;
+  height: 100vh;
+`
+
+const StyledPortfolioItem = styled.div<{ gridArea: string }>`
+  align-items: center;
+  grid-area: ${props => props.gridArea};
+  display: flex;
+  justify-content: center;
+`
 
 const Portfolio = () => {
   return (
@@ -9,7 +23,15 @@ const Portfolio = () => {
       <Helmet>
         <title>Portfolio</title>
       </Helmet>
-      <h1>Portfolio coming...</h1>
+
+      <StyledPortfolioContainer>
+        <StyledPortfolioItem gridArea={'1 / 1 / 2 / 2'}>Over</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'1 / 2 / 2 / 3'}>Twitch</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'1/ 3 / 2 / 4'}>Your company</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 1 / 3 / 2'}>George Mason University</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 2 / 3 / 3'}>Oxford University</StyledPortfolioItem>
+        <StyledPortfolioItem gridArea={'2 / 3 / 3 / 4'}>Current areas of interest</StyledPortfolioItem>
+      </StyledPortfolioContainer>
     </Shell>
   )
 }
