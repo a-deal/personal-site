ea7358a2e6fdefd67ab0fb46d5e61e34bb0ee661
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-16 18:42:09 +0300

    Refactor card layout

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -28,9 +28,11 @@ const StyledCard = styled.div`
   box-shadow: 0 16px 32px 0 rgba(0, 0, 0, 0.08);
   border: 1px solid #dedede;
   border-radius: 8px;
+  display: flex;
+  flex-direction: column;
   height: 352px;
   padding: 16px;
-  width: 376px;
+  width: 648px;
 
   h2 {
     font-size: 18px;
@@ -40,51 +42,34 @@ const StyledCard = styled.div`
 const StyledCardHeader = styled.div`
   align-items: center;
   display: flex;
-  justify-content: space-between;
+  margin-bottom: 16px;
+  margin-right: 16px;
+  white-space: nowrap;
 `
 
 const StyledCardLogo = styled.img`
   border-radius: 8px;
-  height: 50px;
-  width: 50px;
+  height: 75px;
+  margin-right: 16px;
+  width: 75px;
 `
 
-const StyledCardHeaderInfo = styled.div`
-  align-items: flex-end;
+const StyledCardBodyHeader = styled.div`
+  align-items: center;
   display: flex;
+  flex: 0 0 250px;
   flex-direction: column;
+  white-space: nowrap;
 
   p {
-    font-size: 10px;
-  }
-`
-
-const StyledDivider = styled.div`
-  position: relative;
-  height: 1px;
-  margin-top: 16px;
-  margin-bottom: 12px;
-
-  ::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 5%;
-    right: 5%;
-    width: 90%;
-    height: 1px;
-    background-image: linear-gradient(to right, transparent, rgb(101, 101, 101), transparent);
+    font-size: 12px;
   }
 `
 
 const StyledCardBody = styled.div`
+  align-items: center;
+  align-self: flex-end;
   display: flex;
-  flex-direction: column;
-
-  h2 {
-    align-self: center;
-    margin-bottom: 12px;
-  }
 `
 
 const StyledList = styled.ol<{ listImage: string }>`
@@ -111,6 +96,13 @@ const StyledList = styled.ol<{ listImage: string }>`
   }
 `
 
+const StyledDivider = styled.div`
+  border: 1px solid #656565;
+  margin-bottom: 8px;
+  margin-top: 8px;
+  width: 32px;
+`
+
 const Portfolio = () => {
   return (
     <Shell>
@@ -123,14 +115,14 @@ const Portfolio = () => {
           <StyledCard>
             <StyledCardHeader>
               <StyledCardLogo src={OverLogo} />
-              <StyledCardHeaderInfo>
-                <h2>Over Inc.</h2>
-                <p>April ‘19 - present</p>
-              </StyledCardHeaderInfo>
+              <h1>Over Inc.</h1>
             </StyledCardHeader>
-            <StyledDivider />
             <StyledCardBody>
-              <h2>Web Engineer</h2>
+              <StyledCardBodyHeader>
+                <h2>Web Engineer</h2>
+                <StyledDivider />
+                <p>April ‘19 - present</p>
+              </StyledCardBodyHeader>
               <StyledList listImage={EngineerBulletImage}>
                 <li>Led cross-platform initiative to bring Over Teams to parity with our IOS flagship on web.</li>
                 <li>Collaborated on cross-functional teams as Frontend SME to design client-side architectures.</li>
@@ -144,14 +136,14 @@ const Portfolio = () => {
           <StyledCard>
             <StyledCardHeader>
               <StyledCardLogo src={TwitchLogo} />
-              <StyledCardHeaderInfo>
-                <h2>Twitch Inc.</h2>
-                <p>May ‘16 - October ‘17</p>
-              </StyledCardHeaderInfo>
+              <h1>Twitch Inc.</h1>
             </StyledCardHeader>
-            <StyledDivider />
             <StyledCardBody>
-              <h2>Software Engineer</h2>
+              <StyledCardBodyHeader>
+                <h2>Software Engineer</h2>
+                <StyledDivider />
+                <p>May ‘16 - October ‘17</p>
+              </StyledCardBodyHeader>
               <StyledList listImage={EngineerBulletImage}>
                 <li>Introduced OIDC protocol to create Twitch's identity federation for over 15+ million DAUs.</li>
                 <li>Designed client architecture and asset pipelines to reduce latency for a global audience. </li>
@@ -161,62 +153,50 @@ const Portfolio = () => {
             </StyledCardBody>
           </StyledCard>
         </StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'1/ 3 / 2 / 4'}>
-          <StyledCard>
-            <StyledCardHeader>
-              <h2>Your Company...</h2>
-            </StyledCardHeader>
-          </StyledCard>
-        </StyledPortfolioItem>
         <StyledPortfolioItem gridArea={'2 / 1 / 3 / 2'}>
           <StyledCard>
             <StyledCardHeader>
               <StyledCardLogo src={GMULogo} />
-              <StyledCardHeaderInfo>
-                <h2>George Mason University</h2>
-                <p>B.A. Philosophy, 2012</p>
-              </StyledCardHeaderInfo>
+              <h1>George Mason University</h1>
             </StyledCardHeader>
-            <StyledDivider />
-            <StyledList listImage={EducationBulletImage}>
-              <li>Graduated Cum Laude with 3.65 GPA.</li>
-              <li>Concentrated in Analytic, Ancient and Existential Philosophy.</li>
-              <li>Founded and led incipient CrossFit affiliate program for 50+ faculty members and students.</li>
-              <li>Administered fitness and nutritional seminars for 75+ Army ROTC cadets.</li>
-            </StyledList>
+            <StyledCardBody>
+              <StyledCardBodyHeader>
+                <h2>Philosophy, B.A.</h2>
+                <StyledDivider />
+                <p>2012</p>
+              </StyledCardBodyHeader>
+              <StyledList listImage={EducationBulletImage}>
+                <li>Graduated Cum Laude with 3.65 GPA.</li>
+                <li>Concentrated in Analytic, Ancient and Existential Philosophy.</li>
+                <li>Founded and led incipient CrossFit affiliate program for 50+ faculty members and students.</li>
+                <li>Administered fitness and nutritional seminars for 75+ Army ROTC cadets.</li>
+              </StyledList>
+            </StyledCardBody>
           </StyledCard>
         </StyledPortfolioItem>
         <StyledPortfolioItem gridArea={'2 / 2 / 3 / 3'}>
           <StyledCard>
             <StyledCardHeader>
               <StyledCardLogo src={OxfordLogo} />
-              <StyledCardHeaderInfo>
-                <h2>Oxford University</h2>
-                <p>Exchange Student, 2011</p>
-              </StyledCardHeaderInfo>
+              <h1>Oxford University</h1>
             </StyledCardHeader>
-            <StyledDivider />
-            <StyledList listImage={EducationBulletImage}>
-              <li>Engaged in rigorous tutorial system focusing on Philosophy of Science and Modern Philosophy.</li>
-              <li>Developed analytical and critical writing skills under fast-paced deadlines.</li>
-              <li>
-                Collaborated with eminent professors to develop cogent rebuttals to highly nuanced areas within Analytic
-                and Continental Philosophy.
-              </li>
-              <li>Member of St. Catherine’s College and the renowned Oxford Union.</li>
-            </StyledList>
-          </StyledCard>
-        </StyledPortfolioItem>
-        <StyledPortfolioItem gridArea={'2 / 3 / 3 / 4'}>
-          <StyledCard>
-            <StyledCardHeader>
-              <h2>Current Areas of Interest</h2>
-            </StyledCardHeader>
-            <ol>
-              <li>What I'm reading</li>
-              <li>What I'm working on</li>
-              <li>What excites me</li>
-            </ol>
+            <StyledCardBody>
+              <StyledCardBodyHeader>
+                <h2>Philosophy of Science,</h2>
+                <h2>Modern Philosophy</h2>
+                <StyledDivider />
+                <p>2011</p>
+              </StyledCardBodyHeader>
+              <StyledList listImage={EducationBulletImage}>
+                <li>Engaged in rigorous tutorial system focusing on Philosophy of Science and Modern Philosophy.</li>
+                <li>Developed analytical and critical writing skills under fast-paced deadlines.</li>
+                <li>
+                  Collaborated with eminent professors to develop cogent rebuttals to highly nuanced areas within
+                  Analytic and Continental Philosophy.
+                </li>
+                <li>Member of St. Catherine’s College and the renowned Oxford Union.</li>
+              </StyledList>
+            </StyledCardBody>
           </StyledCard>
         </StyledPortfolioItem>
       </StyledPortfolioContainer>
