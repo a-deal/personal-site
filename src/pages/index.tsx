c4ce3af2108d079b7d78b2926773c99a9e873ca3
Andrew Deal <deal.e.andrew@gmail.com>
2019-09-23 16:33:14 +0300

    Adjust vertical offset for top / bottom faces / rename cube to box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,68 +7,76 @@ import { Shell } from '../components/Layout/Shell'
 import Home from '../components/Home'
 import Portfolio from '../components/Portfolio'
 
-const CubeContainer = styled(animated.div)`
+const BoxContainer = styled(animated.div)`
   box-sizing: border-box;
   display: flex;
   height: 100vh;
   width: 100vw;
-  perspective: none;
+  perspective: calc(100vw + 100vh);
 `
 
-const Cube = styled(animated.div)`
+const Box = styled(animated.div)`
   width: 100%;
   height: 100%;
   position: relative;
   margin: auto;
   transform-style: preserve-3d;
-  transform: translateZ(-50vh);
+  transform: translateZ(-50vw);
   transition: transform 1s ease-out;
 `
 
-const CubeFace = styled.div<{ transform: string; background: string }>`
+const BoxFace = styled.div<{ transform: string;height: string; width: string }>`
   border: 1px solid black;
   position: absolute;
-  width: 100vw;
-  height: 100vh;
+  width: ${props => props.width};
+  height: ${props => props.height};
   transform: ${props => props.transform};
-  background: ${props => props.background};
+  background-color: rgba(3, 121, 255, 0.5);
+`
+
+const BoxFaceTopBottom = styled(BoxFace)`
+  top: calc(calc(100vh - 100vw) / 2);
 `
 
 // TODO Set background colors
 const rotations = {
   front: {
-    transform: 'rotateY(0deg) translateZ(50vh)',
-    background: 'hsla(  0, 100%, 50%, .7)',
+    transform: 'rotateY(0deg) translateZ(50vw)',
+    width: '100vw',
+    height: '100vh',
   },
   back: {
-    transform: 'rotateY(180deg) translateZ(50vh)',
-    background: 'hsla(120, 100%, 50%, .7)',
+    transform: 'rotateY(180deg) translateZ(50vw)',
+    width: '100vw',
+    height: '100vh',
   },
   right: {
-    transform: 'rotateY(90deg) translateZ(50vh)',
-    background: 'hsla( 60, 100%, 50%, .7)',
+    transform: 'rotateY(90deg) translateZ(50vw)',
+    width: '100vw',
+    height: '100vh',
   },
   left: {
-    transform: 'rotateY(-90deg) translateZ(50vh)',
-    background: 'hsla(180, 100%, 50%, .7)',
+    transform: 'rotateY(-90deg) translateZ(50vw)',
+    width: '100vw',
+    height: '100vh',
   },
   top: {
     transform: 'rotateX(90deg) translateZ(50vh)',
-    background: ' hsla(240, 100%, 50%, .7)',
+    width: '100vw',
+    height: '100vw',
   },
   bottom: {
     transform: 'rotateX(-90deg) translateZ(50vh)',
-    background: 'background: hsla(300, 100%, 50%, 0.7);',
+    width: '100vw',
+    height: '100vw',
   },
 }
 
 const Index = () => {
   const [flip, setFlipState] = useState(false)
 
-  const springCubeProps = useSpring({
-    transform: ` translateZ(${flip ? '-50vh' : '0px'}) rotateX(${flip ? '-180deg' : '0deg'}) scale3d(${
-      flip ? '-1, -1, 1' : '1,1,1'
-    }) `,
+  const springBoxProps = useSpring({
+    transform: ` translateZ(-50vw) rotateY(${flip ? '180deg' : '0deg'})`,
     config: { duration: 1000 },
   })
 
@@ -84,20 +92,20 @@ const Index = () => {
         <title>{flip ? 'Portfolio' : 'Welcome, ברוך הבא, ようこそ, Bienvenido'}</title>
       </Helmet>
 
-      <CubeContainer>
-        <Cube style={springCubeProps}>
-          <CubeFace {...rotations.front}>
+      <BoxContainer>
+        <Box style={springBoxProps}>
+          <BoxFace {...rotations.front}>
             <Home onFlip={handleFlip} />
-          </CubeFace>
-          <CubeFace {...rotations.back}>
+          </BoxFace>
+          <BoxFace {...rotations.back}>
             <Portfolio />
-          </CubeFace>
-          <CubeFace {...rotations.right}>right</CubeFace>
-          <CubeFace {...rotations.left}>left</CubeFace>
-          <CubeFace {...rotations.top}>top</CubeFace>
-          <CubeFace {...rotations.bottom}>bottom</CubeFace>
-        </Cube>
-      </CubeContainer>
+          </BoxFace>
+          <BoxFace {...rotations.right}>right</BoxFace>
+          <BoxFace {...rotations.left}>left</BoxFace>
+          <BoxFaceTopBottom {...rotations.top}>top</BoxFaceTopBottom>
+          <BoxFaceTopBottom {...rotations.bottom}>bottom</BoxFaceTopBottom>
+        </Box>
+      </BoxContainer>
     </Shell>
   )
 }
