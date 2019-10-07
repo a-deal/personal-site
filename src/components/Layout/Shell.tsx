d62148ae78ee18d37d22a55c843d352697375764
Andrew Deal <deal.e.andrew@gmail.com>
2019-10-07 23:43:23 +0900

    Correct file imports for font assets

--- src/components/Layout/Shell.tsx
+++ src/components/Layout/Shell.tsx
@@ -2,8 +2,8 @@ import React, { PropsWithChildren } from 'react'
 import { Normalize } from 'styled-normalize'
 import { Helmet } from 'react-helmet'
 
-import norwester from './fonts/Norwester.otf'
-import comfortaa from './fonts/Comfortaa_Regular.otf'
+import norwester from '../../styles/fonts/Norwester.otf'
+import comfortaa from '../../styles/fonts/Comfortaa_Regular.otf'
 import { Theme } from '../../styles/Theme'
 import { GlobalStyle } from '../../styles/Global'
 
